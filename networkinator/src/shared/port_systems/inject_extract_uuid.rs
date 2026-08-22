0bab09f48c2f209f460a9ca2cf271f572b19d613
Kevenpvp <76878791+Kevenpvp@users.noreply.github.com>
2026-08-22 12:04:50 -0300

    Rust updated last version

--- networkinator/src/shared/port_systems/inject_extract_uuid.rs
+++ networkinator/src/shared/port_systems/inject_extract_uuid.rs
@@ -3,8 +3,8 @@ use bevy::asset::uuid::Uuid;
 
 pub fn inject_uuid(bytes: Vec<u8>, uuid: Uuid) -> Vec<u8> {
     let mut new_bytes = Vec::with_capacity(16 + bytes.len());
-    new_bytes.extend_from_slice(uuid.as_bytes()); // UUID primeiro
-    new_bytes.extend_from_slice(&bytes);          // depois o payload
+    new_bytes.extend_from_slice(uuid.as_bytes());
+    new_bytes.extend_from_slice(&bytes);
     new_bytes
 }
 
