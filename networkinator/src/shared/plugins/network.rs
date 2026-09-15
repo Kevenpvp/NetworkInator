9ccba2ac3b26fe9f886477eb098e2824a8529737
Kevenpvp <76878791+Kevenpvp@users.noreply.github.com>
2026-09-15 14:46:04 -0300

    Update network.rs

--- networkinator/src/shared/plugins/network.rs
+++ networkinator/src/shared/plugins/network.rs
@@ -546,8 +546,10 @@ impl ServerConnection {
     }
 
     pub fn peer_authenticated(&mut self, current_session_uuid: Uuid, new_session_uuid: Option<Uuid>, peer_uuid: Uuid, instant: Instant){
-        if let Some(new_session_uuid) = new_session_uuid && let Some(old_instant) = self.peers_connected.remove(&current_session_uuid) {
-            self.peers_connected.insert(new_session_uuid,old_instant);
+        if let Some(new_session_uuid) = new_session_uuid && let Some(mut old_connected_infos) = self.peers_connected.remove(&current_session_uuid) {
+            old_connected_infos.peer_uuid = Some(peer_uuid);
+
+            self.peers_connected.insert(new_session_uuid,old_connected_infos);
 
             self.peers_authenticated.insert(peer_uuid,AuthenticatedInfos {
                 instant,
